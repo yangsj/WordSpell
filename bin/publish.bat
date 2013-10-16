6b63aa6f97f582f3f461247488ecd33b8aaac47b
yangsj <yangsj@kingnet.com>
2013-10-16 17:21:49 +0800

    更新目录

---
+++ bin/publish.bat
@@ -0,0 +1,8 @@
+
+del F:\yangsj\WordSpell_Share\client\output
+
+copy F:\yangsj\WordSpell\bin F:\yangsj\WordSpell_Share\client\output
+
+copy F:\yangsj\WordSpell\bin\assets F:\yangsj\WordSpell_Share\client\output\assets
+
+pause
